1c4b751490c87b56f87e459c7d8f226cb3e2b737
James Brink <brink.james@gmail.com>
2017-12-24 00:29:58 +0000

    added default images 21,22,23 and 24

---
+++ build/x86/default/api_24/docker-assets/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Starting Android"
+
+cleanup ()
+{
+  echo "Exiting"
+  vncserver -kill :1
+  pkill -15 emulator
+  exit $?
+}
+
+trap cleanup SIGTERM
+
+vncserver
+emulator @Nexus_5X_API_24 &
+
+IP=`hostname -i`
+socat tcp-listen:5554,bind=$IP,fork tcp:127.0.0.1:5554 &
+socat tcp-listen:5555,bind=$IP,fork tcp:127.0.0.1:5555 &
+adb logcat &
+wait ${!}
